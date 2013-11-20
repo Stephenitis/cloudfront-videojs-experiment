ffd888aeed5e8dc2f901cda86a49edc58b90ddd6
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-11-20 15:57:33 -0800

    copy

--- app/assets/javascripts/holiday.js
+++ app/assets/javascripts/holiday.js
@@ -1,19 +1,17 @@
-$( document ).ready(function() {
-  $("h1").fadeIn( 3000, function() {
-    $("h1").delay(2000).fadeOut(2000, function() {
-      $("h1").html("This was slightly harder</br> than it seamed").fadeIn( 4000, function() {
-        $("h1").delay(2000).fadeOut(2000, function() {
-          $("h1").html("white text on<br/> mysterious background wins!").fadeIn( 4000, function() {
-            $("h1").delay(2000).fadeOut(2000, function() {
-              $("h1").html("Sweet Nightmares<br/> Stephen").append("<br/><small>p.s what kind of <a href='http://www.youtube.com/watch?v=Utu5SnO9PIg'>bear is best?</a></small>").fadeIn( 4000, function() {
-              });
-            });
-          });
-        });
-      });
-    });
-  });
-
-
-});
+// $( document ).ready(function() {
+//   $("h1").fadeIn( 3000, function() {
+//     $("h1").delay(2000).fadeOut(2000, function() {
+//       $("h1").html("This was slightly harder</br> than it seamed").fadeIn( 4000, function() {
+//         $("h1").delay(2000).fadeOut(2000, function() {
+//           $("h1").html("white text on<br/> mysterious background wins!").fadeIn( 4000, function() {
+//             $("h1").delay(2000).fadeOut(2000, function() {
+//               $("h1").html("Sweet Nightmares<br/> Stephen").append("<br/><small>p.s what kind of <a href='http://www.youtube.com/watch?v=Utu5SnO9PIg'>bear is best?</a></small>").fadeIn( 4000, function() {
+//               });
+//             });
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
 
