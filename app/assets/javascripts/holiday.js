156934fb0f58d5edafe861c68a1f045c5323e621
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-11-19 23:20:00 -0800

    changed text

--- app/assets/javascripts/holiday.js
+++ app/assets/javascripts/holiday.js
@@ -1,9 +1,13 @@
 $( document ).ready(function() {
   $("h1").fadeIn( 4000, function() {
     $("h1").delay(2000).fadeOut(2000, function() {
-      $("h1").html("Looking forward to seeing you next").fadeIn( 4000, function() {
+      $("h1").html("Was bored sitting in class...</br> so made this").fadeIn( 4000, function() {
         $("h1").delay(2000).fadeOut(2000, function() {
-          $("h1").html("Sweet Nightmares<br/> Stephen").append("<br/><small>p.s hat kind of bear is best?</small>").fadeIn( 4000, function() {
+          $("h1").html("Looking forward to </br> seeing you next").fadeIn( 4000, function() {
+            $("h1").delay(2000).fadeOut(2000, function() {
+              $("h1").html("Sweet Nightmares<br/> Stephen").append("<br/><small>p.s what kind of bear is best?</small>").fadeIn( 4000, function() {
+              });
+            });
           });
         });
       });
