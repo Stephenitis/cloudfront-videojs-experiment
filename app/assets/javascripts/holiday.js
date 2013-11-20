245b8c84b3716b1e9dad653288e619616b481652
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-11-20 01:23:11 -0800

    copy

--- app/assets/javascripts/holiday.js
+++ app/assets/javascripts/holiday.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     $("h1").delay(2000).fadeOut(2000, function() {
       $("h1").html("This was slightly harder</br> than it seamed").fadeIn( 4000, function() {
         $("h1").delay(2000).fadeOut(2000, function() {
-          $("h1").html("Looking forward to </br> seeing you next").fadeIn( 4000, function() {
+          $("h1").html("white text on<br/> mysterious background wins!").fadeIn( 4000, function() {
             $("h1").delay(2000).fadeOut(2000, function() {
               $("h1").html("Sweet Nightmares<br/> Stephen").append("<br/><small>p.s what kind of <a href='http://www.youtube.com/watch?v=Utu5SnO9PIg'>bear is best?</a></small>").fadeIn( 4000, function() {
               });
